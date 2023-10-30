34e9a4fe67e7daf2fc9e7525b50a7406c820bb58
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-10-31 00:27:43 +0900

    v2.0.2 新增 1. 搜索可以加载更多 修复 1. 服务器错误数据导致的闪退(#34) 2. 保存封面超时导致的闪退 3. 安卓 11 及以上系统浏览漫画时导航栏出现 优化 1. 更新 targetSdkVersion 到 34 2. 搜索支持嵌套滚动 3. 底部按导航栏高度垫高 4. 升级 Gson 库

--- app/src/main/java/top/fumiama/copymanga/ui/vm/ViewMangaActivity.kt
+++ app/src/main/java/top/fumiama/copymanga/ui/vm/ViewMangaActivity.kt
@@ -136,8 +136,11 @@ class ViewMangaActivity : TitleActivityTemplate() {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         else {
             window.setDecorFitsSystemWindows(false)
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-            window.insetsController?.hide(WindowInsets.Type.navigationBars())
+            window.insetsController?.apply {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
         }
     }
 
